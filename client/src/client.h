c49b8c9b6d2b64ed91df424dbe314cd8fb604a22
kyle <kyle@cid.(none)>
2012-09-23 10:43:02 -0400

    Initial files and file directories

---
+++ client/src/client.h
@@ -0,0 +1,24 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+	//PARSE INPUT
+	//MAKE CONNECTION BASED ON THAT
+	//GET LIST FROM SERVER
+	//ADD FILES TO SERVER
+	//GET CONFIRMATION FROM SERVER THAT LIST WAS UPDATED
+	//CLOSE SOCKET
+	//print output
+	return 0;
+}
+#endif
